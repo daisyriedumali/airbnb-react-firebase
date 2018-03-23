db797de4ba25e937d9c46daf1d2d7ffbafcf78d5
Adam Navarro <navarro.adamadrian@gmail.com>
2018-03-23 11:40:02 +0800

    Added room page markup

--- src/components/MakeReservation.js
+++ src/components/MakeReservation.js
@@ -1,11 +1,41 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import * as routes from '../constants/routes';
+import Header from './common/header'
 
 const MakeReservation = props => (
+
   <div>
-  	<Link to={routes.LANDING}>Home</Link>
-  	<h5>Make a reservation!</h5>
+    <Header />
+    <div className="reserve-wrapper width-lg">
+      <h2>Room Name</h2>
+      <div className="top-row width-lg">
+        <div className="booking-form">
+          <form action="">
+            <label htmlFor="">Date</label>
+            <input type="date" />
+
+            <label htmlFor="">Time</label>
+            <input type="time" />
+
+            <button className="book-cta">Book</button>
+          </form>
+        </div>
+        <div className="room-information">
+          <div className="room-photo">
+            <img src="/images/room.jpg" alt="" />
+          </div>
+          <div className="the-room">
+            <h4>The Room</h4>
+            <p>Kogi Cosby sweater ethical squid irony disrupt, organic tote bag gluten-free XOXO wolf typewriter mixtape small batch.</p>
+          </div>
+          <div className="amenities">
+            <h4>Amenities</h4>
+          </div>
+        </div>
+      </div>
+      <div className="calendar-wrapper width-lg"></div>
+    </div>
   </div>
 );
 
