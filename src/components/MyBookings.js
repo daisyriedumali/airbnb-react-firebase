ad393a622f37d44968a801cb826cc1e108ae6ef9
Adam Navarro <navarro.adamadrian@gmail.com>
2018-03-23 16:02:20 +0800

    Fixed cancel booking cta

--- src/components/MyBookings.js
+++ src/components/MyBookings.js
@@ -5,137 +5,137 @@ import { firebase } from '../firebase';
 import moment from 'moment';
 
 class MyBookings extends Component {
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
 
-        this.state = {
-          authUser     : null,
-          bookings     : null,
-          past_bookings: null,
-          future_bookings: null
-        };
+    this.state = {
+      authUser: null,
+      bookings: null,
+      past_bookings: null,
+      future_bookings: null
+    };
 
-        this.fetchMyBookings  = this.fetchMyBookings.bind(this);
-        this.renderMyBookings = this.renderMyBookings.bind(this);
-        this.onDeleteBooking  = this.onDeleteBooking.bind(this);
-    }
+    this.fetchMyBookings = this.fetchMyBookings.bind(this);
+    this.renderMyBookings = this.renderMyBookings.bind(this);
+    this.onDeleteBooking = this.onDeleteBooking.bind(this);
+  }
 
-    componentWillMount() {
-        firebase.auth.onAuthStateChanged(authUser => {
-            this.setState(() => ({ authUser }));
-            this.fetchRooms();
-            this.fetchMyBookings();
-        });
-    }
+  componentWillMount() {
+    firebase.auth.onAuthStateChanged(authUser => {
+      this.setState(() => ({ authUser }));
+      this.fetchRooms();
+      this.fetchMyBookings();
+    });
+  }
 
-    fetchRooms() {
-        db.onceGetRooms().then(snapshot => {
-            this.setState(() => ({rooms : snapshot.val()}));
-        });
-    }
+  fetchRooms() {
+    db.onceGetRooms().then(snapshot => {
+      this.setState(() => ({ rooms: snapshot.val() }));
+    });
+  }
 
-    onDeleteBooking(id) {
-        let uid = (this.state.authUser!=null) ? this.state.authUser.uid : '';
-        db.onceRemoveBooking(id)
-          .then(() => {
-            this.fetchMyBookings();
-          })
-          .catch(error => {
-        });
-    }
+  onDeleteBooking(id) {
+    let uid = (this.state.authUser != null) ? this.state.authUser.uid : '';
+    db.onceRemoveBooking(id)
+      .then(() => {
+        this.fetchMyBookings();
+      })
+      .catch(error => {
+      });
+  }
 
-    fetchMyBookings() {
-        db.onceGetBookingsByUserId(this.state.authUser.uid).then(snapshot => {
-            let time_now = new Date();
-            let timestamp = time_now.getTime();
-            
-            let val = snapshot.val();
-            let bookings = [];
-            Object.keys(val).forEach(function(id) {
-                let _b = val[id];
-                _b.id = id;
-                bookings.push(val[id]);
-            });
+  fetchMyBookings() {
+    db.onceGetBookingsByUserId(this.state.authUser.uid).then(snapshot => {
+      let time_now = new Date();
+      let timestamp = time_now.getTime();
 
-            bookings.sort(function(a, b) {
-                return a.check_in_timestamp - b.check_in_timestamp;
-            });
+      let val = snapshot.val();
+      let bookings = [];
+      Object.keys(val).forEach(function (id) {
+        let _b = val[id];
+        _b.id = id;
+        bookings.push(val[id]);
+      });
 
-            let past_bookings = [];
-            let future_bookings = [];
-            bookings.forEach(function(booking) {
-                if(timestamp > booking.check_in_timestamp) {
-                    past_bookings.push(booking);
-                } else {
-                    future_bookings.push(booking);
-                }
-            });
+      bookings.sort(function (a, b) {
+        return a.check_in_timestamp - b.check_in_timestamp;
+      });
 
-            this.setState(() => ({bookings : snapshot.val(), past_bookings : past_bookings, future_bookings : future_bookings}));
-        });
-    }
-
-    renderMyBookings(type) {
-        if(this.state.bookings != null) {
-            let onDel = this.onDeleteBooking;
-            let _this = this;
-            let bookings = this.state.bookings;
-            let rooms = this.state.rooms;
-            let render_booking = [];
-            
-            this.state[type].forEach(function(booking) {
-                let _rid = booking.room_id;
-
-                Object.keys(rooms).forEach(function(id) {
-                    if(_rid == rooms[id].id) {
-                        render_booking.push(
-                            <div className="room-card">
-                                <div className="room-photo">
-                                  <img src={rooms[id].photos[1]} alt="" />
-                                </div>
-                                <h3>{rooms[id].name}</h3>
-                                <p className="date">{moment(booking.check_in).format('L LTS')}</p>
-                                <p className="time">{moment(booking.check_out).format('L LTS')}</p>
-                                { type == 'future_bookings' &&
-                                  <button onClick={onDel.bind(_this, booking.id)}>X</button> }
-                            </div>
-                        );
-                    }
-                });
-            });
-
-            if(render_booking.length == 0) {
-                render_booking.push(
-                    <div className="room-card">
-                        You have no {type == 'future_bookings' ? 'Upcoming' : 'Past'} Booking
-                    </div>
-                );
-            }
-            return render_booking;
+      let past_bookings = [];
+      let future_bookings = [];
+      bookings.forEach(function (booking) {
+        if (timestamp > booking.check_in_timestamp) {
+          past_bookings.push(booking);
+        } else {
+          future_bookings.push(booking);
         }
-    }
+      });
 
-    render() {
-        // console.log(this.state.authUser != null && this.state.authUser.uid)
-        return (
-            <div>
-              <Header />
-              <div className="bookings-wrapper width-lg">
-                <h2>Upcoming Bookings</h2>
-                <div className="upcoming-wrapper">
-                    {this.renderMyBookings('future_bookings')}
-                </div>
-                
-                <hr />
+      this.setState(() => ({ bookings: snapshot.val(), past_bookings: past_bookings, future_bookings: future_bookings }));
+    });
+  }
 
-                <h2>Past Bookings</h2>
-                <div className="upcoming-wrapper">
-                  {this.renderMyBookings('past_bookings')}
+  renderMyBookings(type) {
+    if (this.state.bookings != null) {
+      let onDel = this.onDeleteBooking;
+      let _this = this;
+      let bookings = this.state.bookings;
+      let rooms = this.state.rooms;
+      let render_booking = [];
+
+      this.state[type].forEach(function (booking) {
+        let _rid = booking.room_id;
+
+        Object.keys(rooms).forEach(function (id) {
+          if (_rid == rooms[id].id) {
+            render_booking.push(
+              <div className="room-card">
+                <div className="room-photo">
+                  <img src={rooms[id].photos[1]} alt="" />
                 </div>
+                <h3>{rooms[id].name}</h3>
+                <p className="date">{moment(booking.check_in).format('L LTS')}</p>
+                <p className="time">{moment(booking.check_out).format('L LTS')}</p>
+                {type == 'future_bookings' &&
+                  <button className="cancel-booking" onClick={onDel.bind(_this, booking.id)}>Cancel Booking</button>}
               </div>
-            </div>
+            );
+          }
+        });
+      });
+
+      if (render_booking.length == 0) {
+        render_booking.push(
+          <div className="room-card">
+            You have no {type == 'future_bookings' ? 'Upcoming' : 'Past'} Booking
+                    </div>
         );
+      }
+      return render_booking;
     }
+  }
+
+  render() {
+    // console.log(this.state.authUser != null && this.state.authUser.uid)
+    return (
+      <div>
+        <Header />
+        <div className="bookings-wrapper width-lg">
+          <h2>Upcoming Bookings</h2>
+          <div className="upcoming-wrapper">
+            {this.renderMyBookings('future_bookings')}
+          </div>
+
+          <hr />
+
+          <h2>Past Bookings</h2>
+          <div className="upcoming-wrapper">
+            {this.renderMyBookings('past_bookings')}
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default MyBookings;
