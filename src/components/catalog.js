6ebf08f777317174e3b507642bd0fccaa5827216
Adam Navarro <navarro.adamadrian@gmail.com>
2018-03-23 10:48:52 +0800

    Added landing wrapper

--- src/components/catalog.js
+++ src/components/catalog.js
@@ -6,46 +6,48 @@ import Header from './common/header'
 const Catalog = () =>
     <div className='landing'>
         <Header />
-        <h2>Rooms</h2>
-        <div className="rooms-wrapper">
-            <div className="rooms-row">
-                <div className="room-card large">
-                    <div className="room-photo">
-                        <img src="/images/room.jpg" alt="" />
+        <div className="landing-wrapper width-lg">
+            <h2>Rooms</h2>
+            <div className="rooms-wrapper">
+                <div className="rooms-row">
+                    <div className="room-card large">
+                        <div className="room-photo">
+                            <img src="/images/room.jpg" alt="" />
+                        </div>
+                        <h3 className="name">Celeste and Honor Hall</h3>
+                        <p className="description">Kogi Cosby sweater ethical squid irony disrupt, organic tote bag gluten-free XOXO wolf typewriter mixtape small batch. </p>
                     </div>
-                    <h3 className="name">Celeste and Honor Hall</h3>
-                    <p className="description">Kogi Cosby sweater ethical squid irony disrupt, organic tote bag gluten-free XOXO wolf typewriter mixtape small batch. </p>
-                </div>
-                <div className="room-card large">
-                    <div className="room-photo">
-                        <img src="/images/room.jpg" alt="" />
+                    <div className="room-card large">
+                        <div className="room-photo">
+                            <img src="/images/room.jpg" alt="" />
+                        </div>
+                        <h3 className="name">Celeste and Honor Hall</h3>
+                        <p className="description">Kogi Cosby sweater ethical squid irony disrupt, organic tote bag gluten-free XOXO wolf typewriter mixtape small batch. </p>
                     </div>
-                    <h3 className="name">Celeste and Honor Hall</h3>
-                    <p className="description">Kogi Cosby sweater ethical squid irony disrupt, organic tote bag gluten-free XOXO wolf typewriter mixtape small batch. </p>
                 </div>
-            </div>
 
-            <div className="rooms-row">
-                <div className="room-card small">
-                    <div className="room-photo">
-                        <img src="/images/room.jpg" alt="" />
+                <div className="rooms-row">
+                    <div className="room-card small">
+                        <div className="room-photo">
+                            <img src="/images/room.jpg" alt="" />
+                        </div>
+                        <h3 className="name">Celeste and Honor Hall</h3>
+                        <p className="description">Kogi Cosby sweater ethical squid irony disrupt, organic tote bag gluten-free XOXO wolf typewriter mixtape small batch. </p>
                     </div>
-                    <h3 className="name">Celeste and Honor Hall</h3>
-                    <p className="description">Kogi Cosby sweater ethical squid irony disrupt, organic tote bag gluten-free XOXO wolf typewriter mixtape small batch. </p>
-                </div>
-                <div className="room-card small">
-                    <div className="room-photo">
-                        <img src="/images/room.jpg" alt="" />
+                    <div className="room-card small">
+                        <div className="room-photo">
+                            <img src="/images/room.jpg" alt="" />
+                        </div>
+                        <h3 className="name">Celeste and Honor Hall</h3>
+                        <p className="description">Kogi Cosby sweater ethical squid irony disrupt, organic tote bag gluten-free XOXO wolf typewriter mixtape small batch. </p>
                     </div>
-                    <h3 className="name">Celeste and Honor Hall</h3>
-                    <p className="description">Kogi Cosby sweater ethical squid irony disrupt, organic tote bag gluten-free XOXO wolf typewriter mixtape small batch. </p>
-                </div>
-                <div className="room-card small">
-                    <div className="room-photo">
-                        <img src="/images/room.jpg" alt="" />
+                    <div className="room-card small">
+                        <div className="room-photo">
+                            <img src="/images/room.jpg" alt="" />
+                        </div>
+                        <h3 className="name">Celeste and Honor Hall</h3>
+                        <p className="description">Kogi Cosby sweater ethical squid irony disrupt, organic tote bag gluten-free XOXO wolf typewriter mixtape small batch. </p>
                     </div>
-                    <h3 className="name">Celeste and Honor Hall</h3>
-                    <p className="description">Kogi Cosby sweater ethical squid irony disrupt, organic tote bag gluten-free XOXO wolf typewriter mixtape small batch. </p>
                 </div>
             </div>
         </div>
