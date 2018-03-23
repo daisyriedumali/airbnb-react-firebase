ba42c08f3d9fbc1289e6112868ac937439a4964f
Daisyrie Dumali <daisyrie.dumali@goabroad.com>
2018-03-23 11:47:25 +0800

    fetched rooms

--- src/components/Room.js
+++ src/components/Room.js
@@ -1,14 +1,30 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import * as routes from '../constants/routes';
 import Header from './common/header'
 
-const Room = ({room}) =>
-    <div className="room-card large">
-        <div className="room-photo">
-            <img src={room.photo} alt="" />
-        </div>
-        <h3 className="name">{room.name}</h3>
-        <p className="description">{room.description}</p>
-    </div>
+class Room extends Component {
+    getPhotos() {
+        let photos = this.props.room.photos;
+        let img = [];
+        Object.keys(photos).forEach(function(key) {
+          var val = photos[key];
+          img.push(<img src={val} key={key}/>);
+        });
+
+        return img;
+    }
+
+    render() {
+        return (
+            <div className="room-card large">
+                <div className="room-photo">
+                    {this.getPhotos()}
+                </div>
+                <h3 className="name">{this.props.room.name}</h3>
+                <p className="description">{this.props.room.description}</p>
+            </div>
+        )
+    }
+}
 export default Room;
--- src/components/catalog.js
+++ src/components/catalog.js
@@ -1,27 +1,62 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import { db } from '../firebase';
 import * as routes from '../constants/routes';
 import Header from './common/header'
 import Room from './Room'
 
-const Catalog = () =>
-    <div className='landing'>
-        <Header />
-        <div className="landing-wrapper width-lg">
-            <h2>Rooms</h2>
-            <div className="rooms-wrapper">
-                <div className="rooms-row">
-                    <Room room={{photo:'room.jpg', name:'Celeste and Honor Hall', description:'Kogi'}}/>
-                    <Room room={{photo:'room.jpg', name:'Celeste and Honor Hall', description:'Kogi'}}/>
-                </div>
-                <div className="rooms-row">
-                    <Room room={{photo:'room.jpg', name:'Celeste and Honor Hall', description:'Kogi'}}/>
-                    <Room room={{photo:'room.jpg', name:'Celeste and Honor Hall', description:'Kogi'}}/>
-                </div>
-            </div>
-            
-        </div>
+class Catalog extends Component {
+    constructor(props) {
+        super(props);
 
-    </div>
+        this.state = {
+          rooms        : []
+        };
+
+        this.getRooms = this.getRooms.bind(this);
+    }
+
+    componentWillMount() {
+        db.onceGetRooms().then(snapshot => {
+            this.setState(() => ({rooms : snapshot.val()}));
+        });
+    }
+
+    getRooms(start, end) {
+        let rooms = this.state.rooms;
+        let roomComp = [];
+        let ctr = 0;
+        Object.keys(rooms).forEach(function(id) {
+            if(ctr >= start && ctr < end) {
+                var val = rooms[id];
+                roomComp.push(<Room room={val} key={id}/>);
+            }
+            ctr++;
+        });
+
+        return roomComp;
+    }
+
+    render() {
+        return (
+            <div className='landing'>
+                <Header />
+                <div className="landing-wrapper width-lg">
+                    <h2>Rooms</h2>
+                    <div className="rooms-wrapper">
+                        <div className="rooms-row">
+                            {this.getRooms(0, 2)}
+                        </div>
+                        <div className="rooms-row">
+                            {this.getRooms(2, 5)}
+                        </div>
+                    </div>
+                    
+                </div>
+
+            </div>
+        );
+    }
+}
 
 export default Catalog;
