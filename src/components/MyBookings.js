d21e1f5137a1b92eb1eab6f3cdcb9f0668686e53
Adam Navarro <navarro.adamadrian@gmail.com>
2018-03-23 12:29:33 +0800

    Added static my bookings page

--- src/components/MyBookings.js
+++ src/components/MyBookings.js
@@ -8,7 +8,74 @@ const MyBookings = ({ props }) => {
       <div className="bookings-wrapper width-lg">
         <h2>Upcoming Bookings</h2>
         <div className="upcoming-wrapper">
-
+          <div className="room-card">
+            <div className="room-photo">
+              <img src="/images/room.jpg" alt="" />
+            </div>
+            <h3>Room Name</h3>
+            <p className="date">March 24, 2018</p>
+            <p className="time">10:00AM - 11:30AM</p>
+          </div>
+          <div className="room-card">
+            <div className="room-photo">
+              <img src="/images/room.jpg" alt="" />
+            </div>
+            <h3>Room Name</h3>
+            <p className="date">March 24, 2018</p>
+            <p className="time">10:00AM - 11:30AM</p>
+          </div>
+          <div className="room-card">
+            <div className="room-photo">
+              <img src="/images/room.jpg" alt="" />
+            </div>
+            <h3>Room Name</h3>
+            <p className="date">March 24, 2018</p>
+            <p className="time">10:00AM - 11:30AM</p>
+          </div>
+          <div className="room-card">
+            <div className="room-photo">
+              <img src="/images/room.jpg" alt="" />
+            </div>
+            <h3>Room Name</h3>
+            <p className="date">March 24, 2018</p>
+            <p className="time">10:00AM - 11:30AM</p>
+          </div>
+        </div>
+        <hr />
+        <h2>Past Bookings</h2>
+        <div className="upcoming-wrapper">
+          <div className="room-card">
+            <div className="room-photo">
+              <img src="/images/room.jpg" alt="" />
+            </div>
+            <h3>Room Name</h3>
+            <p className="date">March 24, 2018</p>
+            <p className="time">10:00AM - 11:30AM</p>
+          </div>
+          <div className="room-card">
+            <div className="room-photo">
+              <img src="/images/room.jpg" alt="" />
+            </div>
+            <h3>Room Name</h3>
+            <p className="date">March 24, 2018</p>
+            <p className="time">10:00AM - 11:30AM</p>
+          </div>
+          <div className="room-card">
+            <div className="room-photo">
+              <img src="/images/room.jpg" alt="" />
+            </div>
+            <h3>Room Name</h3>
+            <p className="date">March 24, 2018</p>
+            <p className="time">10:00AM - 11:30AM</p>
+          </div>
+          <div className="room-card">
+            <div className="room-photo">
+              <img src="/images/room.jpg" alt="" />
+            </div>
+            <h3>Room Name</h3>
+            <p className="date">March 24, 2018</p>
+            <p className="time">10:00AM - 11:30AM</p>
+          </div>
         </div>
       </div>
     </div>
